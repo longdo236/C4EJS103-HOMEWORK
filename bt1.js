61526eaee988d61156d07c3abf2b7bb0bd09cb9b
longdo236 <longgooner236@gmail.com>
2020-11-23 20:28:46 +0700

    btvn day 2

---
+++ bt1.js
@@ -0,0 +1,66 @@
+// câu 1:
+// let dresser =['socks', 'shirts', 'pants']
+// console.log(dresser[2]);
+
+// let odds=['1','3','5','7','9']
+// console.log('Tổng số phần tử của mảng là : '+ odds.length);
+
+// câu 2:
+// const odds=[1,3,5,7,9].reduce(add,0);
+// function add(accumulator, a) {
+//     return accumulator + a;
+// }
+
+// console.log(odds);
+
+// câu 3:
+// var firstName = "Neil";
+// var middleName = "deGrasse";
+// var lastName = "Tyson";
+// var Scientist = [firstName, middleName, lastName];
+// console.log(Scientist);
+
+//câu 4+5:
+//  let people = ["Teddy", "Cathy","Bobby"];
+//  console.log(people);
+//  console.log(people[1]="Petter");
+//  let newLength = people.push("Barry");
+//  console.log(people);
+
+// // câu 6: 
+// let people = ["Teddy", "Cathy","Bobby"];
+// console.log(people);
+// let newLength = people.unshift("Alice");
+// console.log(people);
+
+// câu 7:
+// let people = ["Teddy", "Petter","Bobby"];
+//  console.log(people);
+//  people.splice(2,0, 'John', 'James');
+//  console.log(people);
+//  console.log(people[4]= ["Jame",27]);
+//  people.splice(2,4);
+//  console.log(people);
+
+ // câu 8:
+//  let people = ["Teddy", "Petter", "John", "James", "Bobby"];
+//  console.log(people[3]= ["Jame",27]);
+//  console.log(people);
+
+// câu 9:
+// let people = ["Teddy", "Petter", "John",["Jame",27] , "Bobby"];
+// people.splice(1,1);
+// console.log(people);
+// people.splice(2,1);
+// console.log(people);
+
+// câu 10:
+//  let people = ["Teddy", "Petter", "John", "James", "Bobby"];
+//  let rand = people[Math.floor(Math.random() * people.length)];
+// console.log(rand);
+// console.log(people);
+
+//câu 11:
+
+let a = "https://mindx.edu.vn/course/code-for-everyone";
+ 
